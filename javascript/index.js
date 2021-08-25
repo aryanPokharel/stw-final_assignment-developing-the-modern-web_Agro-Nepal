b0ee325f86e3a33162f972953468c92aa274a36b
Aryan Pokharel <aryanpokharel417@gmail.com>
2021-08-25 22:02:22 +0545

    Second commit Folder management

---
+++ javascript/index.js
@@ -0,0 +1,22 @@
+//this js section is just for trial purpose
+
+var name='Aryan';
+console.log('name before section is',name)
+
+{
+    let name = 'Mr. Aryan'
+    console.log('name inside section is',name)
+}
+console.log('name after section is',name)
+
+
+let Name = 'Aryan'
+let age = 20;
+Object
+ person = {
+    fname: 'Aryan',
+    lname: 'Pokharel',
+    address: 'Jhapa'
+}
+
+console.log(person.fname, person.lname, person.address)
