8b79bea3a33f5120148beb998511d006b2e6a5b2
Aryan Pokharel <aryanpokharel417@gmail.com>
2021-08-26 23:17:08 +0545

    Fourth commit - Footer added and minor css modifications

--- javascript/feedbacks.js
+++ javascript/feedbacks.js
@@ -3,14 +3,30 @@
 const btn = document.querySelector('.button');
 const userName = document.querySelector('#name');
 const userEmail = document.querySelector('#email');
-
-btn.addEventListener('click', feedbackGiven);
-
+const hiddenDiv = document.querySelector('.hidden-div');
 
 
+btn.addEventListener('click', btnClicked);
 
-function feedbackGiven(e){
+
+
+
+function btnClicked(e){
     e.preventDefault();
-    console.log("Name :", userName.value);
-    console.log("Email :", userEmail.value);
+    
+    if (userName.value === '' || userEmail.value === ''){
+        hiddenDiv.innerHTML = "<h2> Please fill out all the credentials! </h2>";
+        setTimeout(() => hiddenDiv.remove(), 5000);
+        
+
+    }
+    else{
+        hiddenDiv.innerText = `Thank You   ${userName.value}. We will reach you via your email : ${userEmail.value}`;
+        setTimeout(() => hiddenDiv.remove(), 5000);
+    }
+
+    //Clearing the fields
+    userName.value = '';
+    userEmail.value = '';
+    
 }
