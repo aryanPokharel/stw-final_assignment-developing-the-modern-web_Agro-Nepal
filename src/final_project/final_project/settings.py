caf14104f61540c3cdf91dac6fcccdca25d9d7eb
Aryan Pokharel <aryanpokharel417@gmail.com>
2021-09-21 00:53:49 +0545

    Orders table created

--- src/final_project/final_project/settings.py
+++ src/final_project/final_project/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'students',
     'remarks',
     'insiders',
+    'orders',
 ]
 
 MIDDLEWARE = [
---
+++ src/final_project/orders/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from datetime import date
+from django.utils.timezone import now
+
+
+# Create your models here.
+class Order(models.Model):
+    now = now()
+
+    order_id = models.BigAutoField(primary_key=True)
+    item_type = models.CharField(max_length=200, null=False)
+    item_name = models.CharField(max_length=200)
+    customer_name = models.CharField(max_length=200)
+    customer_email = models.EmailField(max_length=200)
+    order_date = models.DateField(auto_now_add=True)
+    total_price = models.FloatField(default=0)
